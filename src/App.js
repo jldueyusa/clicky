a51c24dbac541b93989515c4e038e9723efe9d38
Jennifer Duey <jlduey@gmail.com>
2019-07-30 16:36:52 -0400

    added images

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
     const newScore = this.state.currentScore + 1;
     this.setState({
       currentScore: newScore,
-      correctIncorrect: "You guessed correctly!"
+      correctIncorrect: "You guessed right!"
     });
     if (newScore >= this.state.topScore) {
       this.setState({ topScore: newScore });
@@ -56,7 +56,7 @@ class App extends Component {
     this.setState({
       currentScore: 0,
       topScore: this.state.topScore,
-      correctIncorrect: "You guessed incorrectly!",
+      correctIncorrect: "You guessed wrong!",
       clicked: []
     });
     this.handleShuffle();
@@ -71,14 +71,14 @@ class App extends Component {
     return (
       <Wrapper>
         <Nav
-          title="React Clicky Game"
+          title="The Clicky Memory Game"
           score={this.state.currentScore}
           topScore={this.state.topScore}
           correctIncorrect={this.state.correctIncorrect}
         />
 
         <Title>
-          Click on an image to earn points, but don't click on any more than once!
+          Match the images. The fewer the turns the higher the score
         </Title>
         <Container>
           <Row>
@@ -119,3 +119,113 @@ export default App;
 // );
 
 // export default Card;
+
+// import React, { PureComponent } from 'react';
+// import Header from './components/header/Header';
+// import Card from './components/card/Card';
+// import GameOver from './components/card/GameOver';
+
+// import './styles/main.css';
+
+// class App extends PureComponent {
+
+//   state = { 
+//     isFlipped: Array(16).fill(false),
+//     shuffledCard: App.duplicateCard().sort(() => Math.random() - 0.5),
+//     clickCount: 1,
+//     prevSelectedCard: -1,
+//     prevCardId: -1
+//   };
+
+//   static duplicateCard = () => {
+//     return [0,1,2,3,4,5,6,7].reduce((preValue, current, index, array) => {
+//       return preValue.concat([current, current])
+//     },[]);
+//   };
+// JEN THIS IS WHERE IT FLIPS
+//   handleClick = event => {
+//     event.preventDefault();
+//     const cardId = event.target.id;
+//     const newFlipps = this.state.isFlipped.slice();
+//     this.setState({
+//         prevSelectedCard: this.state.shuffledCard[cardId],
+//         prevCardId: cardId
+//     });
+
+//     if (newFlipps[cardId] === false) {
+//       newFlipps[cardId] = !newFlipps[cardId];
+//       this.setState(prevState => ({ 
+//         isFlipped: newFlipps,
+//         clickCount: this.state.clickCount + 1
+//       }));
+
+//       if (this.state.clickCount === 2) {
+//         this.setState({ clickCount: 1 });
+//         const prevCardId = this.state.prevCardId;
+//         const newCard = this.state.shuffledCard[cardId];
+//         const previousCard = this.state.prevSelectedCard;
+
+//         this.isCardMatch(previousCard, newCard, prevCardId, cardId);
+//       }
+//     }
+//   };
+//JEN THIS IS WHERE IT CHECKS TO SEE IF THEY MATCH
+//   isCardMatch = (card1, card2, card1Id, card2Id) => {
+//     if (card1 === card2) {
+//       const hideCard = this.state.shuffledCard.slice();
+//       hideCard[card1Id] = -1;
+//       hideCard[card2Id] = -1;
+//       setTimeout(() => {
+//         this.setState(prevState => ({
+//           shuffledCard: hideCard
+//         }))
+//       }, 1000);
+//     } else {
+//       const flipBack = this.state.isFlipped.slice();
+//       flipBack[card1Id] = false;
+//       flipBack[card2Id] = false;
+//       setTimeout(() => {
+//         this.setState(prevState => ({ isFlipped: flipBack }));
+//       }, 1000);
+//     }
+//   };
+
+//   restartGame = () => {
+//     this.setState({
+//       isFlipped: Array(16).fill(false),
+//       shuffledCard: App.duplicateCard().sort(() => Math.random() - 0.5),
+//       clickCount: 1,
+//       prevSelectedCard: -1,
+//       prevCardId: -1
+//     });
+//   };
+
+//   isGameOver = () => {
+//     return this.state.isFlipped.every((element, index, array) => element !== false);
+//   };
+
+//   render() {
+//     return (
+//      <div>
+//        <Header restartGame={this.restartGame} />
+//        { this.isGameOver() ? <GameOver restartGame={this.restartGame} /> :
+//        <div className="grid-container">
+//           {
+//             this.state.shuffledCard.map((cardNumber, index) => 
+//               <Card
+//                 key={index} 
+//                 id={index} 
+//                 cardNumber={cardNumber} 
+//                 isFlipped={this.state.isFlipped[index]} 
+//                 handleClick={this.handleClick}     
+//               />
+//             )
+//           }
+//         </div>
+//        }
+//      </div>
+//     );
+//   }
+// }
+
+// export default App;
